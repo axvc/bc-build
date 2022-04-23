279245afcef703442f4fbb6311b56ed4b78f8b14
Artem Zverev <undemol133@gmail.com>
2022-04-23 19:51:04 +0400

    feat: singing transactions

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,4 +1,6 @@
 const SHA256 = require("crypto-js/sha256");
+const EC = require('elliptic').ec;
+const ec =  new EC('secp256k1');
 
 class Transaction {
 
@@ -6,6 +8,31 @@ class Transaction {
     this.fromAddress = fromAddress;
     this.toAddress = toAddress;
     this.amount = amount;
+  }
+
+  calculateHash() {
+    return SHA256(this.fromAddress + this.toAddress + this.amount).toString()
+  }
+
+  signTransaction(signingKey) {
+    if (signingKey.getPublic('hex') !== this.fromAddress) {
+      throw new Error('You cannot sign transactions for other wallets!')
+    }
+
+    const hashTx = this.calculateHash();
+    const sig = signingKey.sign(hashTx, 'base64');
+    this.signature = sig.toDER('hex')
+  }
+
+  isValid() {
+    if (this.fromAddress === null) return true;
+
+    if (!this.signature || this.signature.length === 0) {
+      throw new Error("No signature in this transaction");
+    }
+
+    const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+    return publicKey.verify(this.calculateHash(), this.signature);
   }
 
 }
@@ -21,7 +48,7 @@ class Block {
   }
 
   calculateHash() {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString()
+    return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString()
   }
 
   mineBlock(difficulty) {
@@ -32,13 +59,23 @@ class Block {
     console.log("Block mined: " + this.hash)
   }
 
+  hasValidTransactions() {
+    for (const tx of this.transactions) {
+      if (!tx.isValid()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
 }
 
 class Blockchain {
 
   constructor() {
     this.chain = [this.createGenesisBlock()];
-    this.difficulty = 5;
+    this.difficulty = 4;
     this.pendingTransactions = [];
     this.miningReward = 100;
   }
@@ -52,18 +89,28 @@ class Blockchain {
   }
 
   minePendingTransactions(miningRewardAddress) {
-    let block = new Block(Date.now(), this.pendingTransactions);
+    const rewardTx = new Transaction(null, miningRewardAddress, this.miningReward);
+    this.pendingTransactions.push(rewardTx);
+
+    let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
     block.mineBlock(this.difficulty);
 
     console.log("Block successfully mined!");
     this.chain.push(block);
 
-    this.pendingTransactions = [
-      new Transaction(null, miningRewardAddress, this.miningReward)
-    ]
+    this.pendingTransactions = []
   }
 
-  createTransaction(transaction) {
+  addTransaction(transaction) {
+
+    if (!transaction.fromAddress || !transaction.toAddress) {
+      throw new Error("Transaction must include form and to address");
+    }
+
+    if (!transaction.isValid()) {
+      throw new Error("Cannot add invalid transaction to chain");
+    }
+
     this.pendingTransactions.push(transaction);
   }
 
@@ -90,16 +137,20 @@ class Blockchain {
       const currentBlock = this.chain[i];
       const previousBlock = this.chain[i - 1];
 
+      if (previousBlock.hash !== currentBlock.previousHash) {
+        return false;
+      }
+
+      if (!currentBlock.hasValidTransactions()) {
+        return false;
+      }
+
       if (currentBlock.hash !== currentBlock.calculateHash()) {
         return false;
       }
-
-      if (currentBlock.previousHash !== previousBlock.hash) {
-        return false;
-      }
-
-      return true;
     }
+
+    return true;
   }
 
 }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
 const { Blockchain, Transaction } = require('./blockchain')
+const EC = require('elliptic').ec;
+const ec =  new EC('secp256k1');
 
+const myKey = ec.keyFromPrivate("1860ffe02ae59575702b8a6410391d5c3f09fc42247eee02da1fb725f92e408b");
+const myWalletAddress = myKey.getPublic('hex');
+
+const sCoin = new Blockchain()
+
+const tx1 =  new Transaction(myWalletAddress, 'some publicKey', 10);
+tx1.signTransaction(myKey);
+sCoin.addTransaction(tx1);
+
+console.log("\n Starting the miner...")
+sCoin.minePendingTransactions(myWalletAddress)
+
+console.log('\n Balance of axvc is', sCoin.getBalanceOfAddress(myWalletAddress))
+
+console.log('Is chain valid', sCoin.isChainValid())
